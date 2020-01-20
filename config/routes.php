c8bff37333c404205dab613e3c4997e728ba8e0f
Ashura <the.lucas2016@gmail.com>
2020-01-20 15:35:30 -0200

    :fire: Remove contact page

--- config/routes.php
+++ config/routes.php
@@ -1,15 +1,8 @@
 <?php
 
-use App\Controllers\ContactController;
 use App\Controllers\HomeController;
-use Slim\Routing\RouteCollectorProxy;
 use Slim\App;
 
-return function (App $app) {
+return function ($app) {
     $app->get('/', HomeController::class . ':index');
-
-    $app->group('/contact', function (RouteCollectorProxy $group) {
-        $group->get('/', ContactController::class . ':index');
-        $group->post('/', ContactController::class . ':store');
-    });
 };
--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -2,15 +2,12 @@
 
 namespace App\Controllers;
 
-use Slim\Http\Response;
-use Slim\Http\ServerRequest;
-
 class HomeController extends Controller
 {
     /**
      * Show index page.
      */
-    public function index(ServerRequest $request, Response $response): Response
+    public function index($request, $response)
     {
         return $this->view->render($response, 'welcome.twig');
     }
